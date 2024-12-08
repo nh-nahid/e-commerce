e342af3fb859c1ec634747e6f0381d7c322d86f5
Nahid Hossain <nahid4510@gmail.com>
2024-12-08 23:58:33 +0600

    off canvas

--- assets/js/common.js
+++ assets/js/common.js
@@ -6,3 +6,28 @@ const logInMenuItem = document.querySelector('#login-item')
 if(isUserLoggedIn()){
     logInMenuItem.style.display = 'none'
 }
+
+const cartBtn = document.querySelector('.view-cart');
+const offCanvasContainer = document.querySelector('.off-canvas');
+const offCanvasOverlay = document.querySelector('.off-canvas-overlay');
+const offCanvasClose = document.querySelector('.close-icon');
+
+
+cartBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    offCanvasContainer.classList.add('show-off-canvas')
+    offCanvasOverlay.classList.add('show-off-canvas-overlay')
+    
+});
+offCanvasOverlay.addEventListener('click',function(e){
+    e.preventDefault()
+    offCanvasContainer.classList.remove('show-off-canvas')
+    offCanvasOverlay.classList.remove('show-off-canvas-overlay')
+
+})
+offCanvasClose.addEventListener('click',function(e){
+    e.preventDefault()
+    offCanvasContainer.classList.remove('show-off-canvas')
+    offCanvasOverlay.classList.remove('show-off-canvas-overlay')
+
+})
