af2700d4afc8d228bf5c59cad0992c984a4f4087
nh-nahid <nh-nahid@users.noreply.github.com>
2024-12-12 17:53:39 +0000

    deploy: 21b3adbf404484c7f7b9a866e2a42a211dc43360

--- assets/shop-DtoxLExJ.js
+++ assets/shop-DtoxLExJ.js
@@ -1,4 +1,4 @@
-import{g as Ct,a as Pt}from"./style-Ck-TGNpc.js";const G=document.querySelector(".right"),At=(t,e,a,s,l,h)=>` <div class="product-container">
+import{g as Ct,a as Pt}from"./style-CUNtHX98.js";const G=document.querySelector(".right"),At=(t,e,a,s,l,h)=>` <div class="product-container">
               <a class="product-link" href="/e-commerce/pages/single.html?id=${+t}"><div class="product-img"><img src="${e}" alt=""></div></a>
              <a class="product-link" href="/e-commerce/pages/single.html?id=${+t}"> <div class="product-title">${a}</div></a>
               <div class="product-rating">${s.rate}</div>
