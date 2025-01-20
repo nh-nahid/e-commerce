2d470cfa5266eb0d49f296b36151c984de8bab80
nh-nahid <nh-nahid@users.noreply.github.com>
2025-01-20 18:04:36 +0000

    deploy: 242233c63e3fdeab3615ae9795098217c0fd203f

--- assets/shop-_uCeXZWc.js
+++ assets/shop-_uCeXZWc.js
@@ -1,4 +1,4 @@
-import{g as Ct,a as Pt}from"./style-DLTVkO4q.js";const G=document.querySelector(".right"),At=(t,e,a,s,l,h)=>` <div class="product-container">
+import{g as Ct,a as Pt}from"./style-C5kXScU2.js";const G=document.querySelector(".right"),At=(t,e,a,s,l,h)=>` <div class="product-container">
               <a class="product-link" href="single.html?id=${+t}"><div class="product-img"><img src="${e}" alt=""></div></a>
              <a class="product-link" href="single.html?id=${+t}"> <div class="product-title">${a}</div></a>
               <div class="product-rating">${s.rate}</div>
